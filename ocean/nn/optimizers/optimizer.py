913cfd18d68102061443d7b0bfad1e9cb706a2c2
kale <42823429+oelin@users.noreply.github.com>
2023-03-27 00:14:08 +0100

    Create optimizer.py

---
+++ ocean/nn/optimizers/optimizer.py
@@ -0,0 +1,10 @@
+@dataclass
+class Optimizer:
+    parameters: tuple
+
+    def zero_grads(self):
+        for parameter in self.parameters:
+            parameter.grad *= 0.
+    
+    def step(self):
+        raise NotImplementedError()
